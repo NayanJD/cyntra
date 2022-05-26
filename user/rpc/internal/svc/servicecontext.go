0d19a6d50c4f26a69117d62f3e73c8ce76ac6e32
Nayan Jyoti Das <nayanj@geekyants.com>
2022-05-26 10:51:44 +0530

    Add database models and migrations sql.

--- user/rpc/internal/svc/servicecontext.go
+++ user/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "cyntra/user/rpc/internal/config"
+import (
+	"cyntra/user/rpc/internal/config"
+	"cyntra/user/rpc/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	UserModel model.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		UserModel: model.NewUserModel(sqlx.NewSqlConn("pg", c.DataSource), c.Cache),
 	}
 }
