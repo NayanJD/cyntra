b77d2893f2ba0d8d55bc21e8cc1e05a4222a5277
Nayan Das <dastms@gmail.com>
2022-06-12 23:12:25 +0530

    Add logic for register handler

--- gateway/api/internal/logic/registerlogic.go
+++ gateway/api/internal/logic/registerlogic.go
@@ -2,12 +2,14 @@ package logic
 
 import (
 	"context"
+	"time"
 
 	"cyntra/gateway/api/internal/svc"
 	"cyntra/gateway/api/internal/types"
 	"cyntra/user/rpc/types/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type RegisterLogic struct {
@@ -24,18 +26,33 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 	}
 }
 
-func (l *RegisterLogic) Register(req *types.RegisterReq) (resp *types.RegisterReq, err error) {
+func (l *RegisterLogic) Register(req *types.RegisterReq) (*types.RegisterResp, error) {
 	// todo: add your logic here and delete this line
-	resp, err := l.svcCtx.UserSvc.RegisterUser(l.ctx, &user.RegisterRequest{
-		Username:  req.Username,
-		FirstName: req.First_name,
-	})
+
+	dobTime, err := time.Parse(time.RFC3339, req.Dob)
+
 	if err != nil {
 		return nil, err
 	}
 
-	return &types.ExpandResp{
-		Url: resp.Url,
+	resp, err := l.svcCtx.UserSvc.RegisterUser(l.ctx, &user.RegisterRequest{
+		Username:  req.Username,
+		FirstName: req.First_name,
+		LastName:  req.Last_name,
+		Gender:    req.Gender,
+		Dob:       timestamppb.New(dobTime),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.RegisterResp{
+		Id:         resp.Id,
+		Username:   resp.Username,
+		First_name: resp.FirstName,
+		Last_name:  resp.LastName,
+		Gender:     resp.Gender,
+		Dob:        resp.Dob.String(),
 	}, nil
-	return
 }
